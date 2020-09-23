2b83bcbf4c75b786b293c21b592a8756b92cd508
Eugene <openmind1377@gmail.com>
2020-09-23 16:00:29 +0300

    Updates

---
+++ precache-manifest.4b02f4af525dbc4752663b7d17b1625c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fe12eff388a6b284f16f042238152b",
+    "url": "/blog_platform/index.html"
+  },
+  {
+    "revision": "e8057422ffcbdd9b9445",
+    "url": "/blog_platform/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "4a5b31c5974337a70fe5",
+    "url": "/blog_platform/static/css/main.ef4ebcd5.chunk.css"
+  },
+  {
+    "revision": "e8057422ffcbdd9b9445",
+    "url": "/blog_platform/static/js/2.3c45a4ae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/blog_platform/static/js/2.3c45a4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5b31c5974337a70fe5",
+    "url": "/blog_platform/static/js/main.127e8b49.chunk.js"
+  },
+  {
+    "revision": "f615fb0ebbaf7e7aaed3",
+    "url": "/blog_platform/static/js/runtime-main.b2020ca4.js"
+  }
+]);
